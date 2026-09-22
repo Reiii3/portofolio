2787e61c50cd9e545844006493741affb695ad2e
Reiii3 <rizamaulanaa3@gmail.com>
2026-09-22 21:18:20 +0700

    Penambahan foto di setiap project yang sebelum nya menggunakan foto dari project game corner dan beberapa penyesuaian di fitur translate dan lain nya

--- src/components/layouts/Projects..tsx
+++ src/components/layouts/Projects..tsx
@@ -1,8 +1,11 @@
+"use client"
 import Image from "next/image";
 import { StaticImageData } from "next/image";
-import IconAxVision from "../../img/icon_axvision_2.webp";
+import AxVision from "../../img/axvision_hawk.webp";
 import RedCorner from "../../img/red_corner_project.webp";
+import AxManager from "../../img/axmanager_society.webp"
 import { FaGithub } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
 
 interface Project {
     title: string;
@@ -12,6 +15,7 @@ interface Project {
     tags: string[];
     liveUrl?: string;
     githubUrl?: string;
+    customLink?: string;
 }
 
 const projects: Project[] = [
@@ -20,9 +24,9 @@ const projects: Project[] = [
         category: "TOOLS",
         description:
             "Tools Android system optimizer yang saya kembangkan sendiri sebagai solo developer. Mengoptimalkan performa sistem Android yang dikenal berat dan boros RAM, terutama di perangkat Xiaomi/POCO/Redmi/Infinix/TECNO.",
-        image: RedCorner,
+        image: AxVision,
         tags: ["Bash", "JavaScript", "Android", "Root", "KernelSU", "AxManager", "KernelSU API", "Vite.js", "React"],
-        githubUrl: "https://github.com/username/axvision",
+        customLink: "https://www.magiskmodule.com/axvision",
     },
     {
         title: "Website Game Corner",
@@ -38,18 +42,19 @@ const projects: Project[] = [
         category: "WEB",
         description:
             "Dikembangkan dari tugas sekolah Pak Tri Gunawan. AxManager adalah tools Android untuk modifikasi ringan (non-root) maupun berat (root). Saya jadi salah satu plugin developer di aplikasi ini (5 plugin, 3 di antaranya sudah tidak mendapat update). Website ini mengenalkan AxManager ke pengguna baru dan komunitas.",
-        image: RedCorner,
+        image: AxManager,
         tags: ["React", "JavaScript", "Vite.js", "Tailwind CSS", "Play Store Scraper API", "Firebase"],
         liveUrl: "https://debug.game-corner.pages.dev/",
     },
 ];
 
 export default function Projects() {
+    const {t} = useTranslation()
     return (
         <section id="proyek" className="px-6 py-20 md:px-10 md:py-28 border-t">
             <div className="mb-10">
-                <h2 className="text-3xl font-bold">Various Projects of Mine</h2>
-                <p className="text-sm text-muted/80 max-w-110">A collection of all the software projects, tools, and various projects I have worked on during my time as a software developer</p>
+                <h2 className="text-3xl font-bold">{t("techStack.title")}</h2>
+                <p className="text-sm text-muted/80 max-w-110">{t("techStack.desc")}</p>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {projects.map((project) => (
@@ -83,6 +88,17 @@ function ProjectCard({ project }: { project: Project }) {
         </a>
     ) : null;
 
+    const customLink = project.customLink ? (
+        <a
+            href={project.customLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-md font-semibold bg-primary text-on-secondary px-4 py-2 rounded-full"
+        >
+            Magisk Module
+        </a>
+    ) : null
+
     return (
         <div className="group transition-all duration-300">
             <div className="flex flex-col gap-4">
@@ -115,6 +131,7 @@ function ProjectCard({ project }: { project: Project }) {
                 <div className="flex gap-3 mt-1">
                     {githubLink}
                     {liveLink}
+                    {customLink}
                 </div>
             </div>
         </div>
--- src/components/layouts/TechStack.tsx
+++ src/components/layouts/TechStack.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import type { StaticImageData } from "next/image";
 
@@ -15,6 +16,7 @@ import Next from "../../img/next-dot-js-svgrepo-com.svg";
 import KernelSU from "../../img/kernelsu.png";
 import Git from "../../img/git-svgrepo-com.svg";
 import Github from "../../img/github-142-svgrepo-com.svg";
+import { useTranslation } from "react-i18next";
 
 interface CardProps {
     label: string;
@@ -22,6 +24,7 @@ interface CardProps {
 }
 
 export default function TechStack() {
+    const {t} = useTranslation()
     const listStackLanguage: CardProps[] = [
         { label: "Kotlin", icon: Kotlin },
         { label: "Bash", icon: Bash },
@@ -45,11 +48,11 @@ export default function TechStack() {
     return (
         <section id="stack" className="bg-[url(/coding.webp)] bg-no-repeat bg-cover">
             <div className="backdrop-blur-sm px-6 py-20 md:px-10 md:py-28">
-                <div className="flex items-center flex-col md:flex-row md:gap-2 gap-4.5">
+                <div className="flex items-center flex-col md:flex-row  md:justify-between md:gap-2 gap-4.5">
                     <div className="flex flex-col gap-1.5 md:max-w-85">
-                        <h1 className="text-3xl font-bold">Programming Language Stack</h1>
+                        <h1 className="text-3xl font-bold">{t("techStack.language.title")}</h1>
                         <p className="text-sm text-muted/80">
-                            Inilah bahasa pemrograman yang sudah saya kuasai selama 5 tahun terakhir ini dan semenjak saya mengenal coding di kelas 1 smp, ada sekitar 8 bahasa pemrograman.
+                            {t("techStack.language.desc")}
                         </p>
                     </div>
                     <div className="p-3 border-l-3 rounded-r-xl flex flex-wrap gap-2 border-green-400 bg-surface-container/80">
@@ -59,11 +62,11 @@ export default function TechStack() {
                     </div>
                 </div>
 
-                <div className="flex items-center flex-col md:flex-row-reverse md:gap-2 gap-4 mt-8">
+                <div className="flex items-center flex-col md:flex-row md:justify-between md:gap-2 gap-4 mt-8">
                     <div className="flex flex-col gap-1.5 md:max-w-85">
-                        <h1 className="text-3xl font-bold">Framework Stack</h1>
+                        <h1 className="text-3xl font-bold">{t("techStack.framework.title")}</h1>
                         <p className="text-sm text-muted/80">
-                            Inilah framework daan Tools yang sudah saya kuasai selama 5 tahun terakhir ini dan semenjak saya mengenal coding di kelas 1 smp, ada sekitar 6 framework.
+                            {t("techStack.framework.desc")}
                         </p>
                     </div>
                     <div className="p-3 border-l-3 rounded-r-xl flex flex-wrap gap-2 border-green-400 bg-surface-container/80">
